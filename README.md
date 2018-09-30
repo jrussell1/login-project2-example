cc17b4074b5a85711c703b07402923d9b85af20e
Jordan Russell <jordanrussell1@gmail.com>
2018-09-29 20:32:09 -0400

    working

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # login-project2-example
+
+## NPM
+1. body-parser
+2. ejs
+3. express
+4. express-session
+5. mysql
+6. req-flash
---
+++ seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO `test`.`users` (`id`, `first_name`, `last_name`, `user_name`, `password`) VALUES ('1', 'jordan', 'russell', 'jordanrussell', 'password');
